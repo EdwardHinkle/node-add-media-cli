0cff07ed4fcb77012dcad0ca36e2c7a44ff51cdb
Edward Hinkle <eddie@meetmaestro.com>
2017-04-19 18:56:38 -0400

    convert to a more proper CLI app

--- build/index.js
+++ build/index.js
@@ -1,24 +1,37 @@
+#!/usr/bin/env node
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var moment = require("moment");
 var fs = require("fs");
 var _ = require("lodash");
+var program = require("commander");
 var config = require('../config.json');
 var MovieDB = require('moviedb')(config.moviedb_key);
 var defaultWatchStatus = "finished";
-var postCategory, watchStatus, watchName, imdbId, watchUrl, watchImageUrl;
-var searchType = process.argv[2];
-var searchTitle = process.argv[3];
-if (searchType == "movie") {
-    postCategory = process.argv[4] ? process.argv[4].toLowerCase() : undefined;
-    watchStatus = process.argv[5] ? process.argv[5].toLowerCase() : defaultWatchStatus;
-}
-else if (searchType == "tv") {
-    var seasonId = process.argv[4];
-    var episodeId = process.argv[5];
-    postCategory = process.argv[6] ? process.argv[6].toLowerCase() : undefined;
-    watchStatus = process.argv[7] ? process.argv[7].toLowerCase() : defaultWatchStatus;
-}
+program.option('-t, --type <type>', 'What type of watch post it is')
+    .option('-n, --name <name>', 'The name of what is being watched')
+    .option('-s, --season <season>', 'The season number of a tv show', parseInt)
+    .option('-e, --episode <episode>', 'The episode number of a tv show', parseInt)
+    .option('-S, --status <status>', 'The status of the watch post: interested, inprogress or finished', /^(interested|inprogress|finished)$/i, defaultWatchStatus)
+    .option('--private', 'Mark this post as a private post')
+    .option('-m, --message <message>', 'This is the message you want to put in the content of the post')
+    .option('-c, --category <category>', 'These are the categories you want to attach to the post', function list(val) {
+    return val.toLowerCase().split(',');
+})
+    .option('-d, --date [date]', 'This is the date of the watch post', function datetime(val) {
+    return moment(val, "MMM D, YYYY h:mm A");
+})
+    .parse(process.argv);
+var watchName, imdbId, watchUrl, watchImageUrl;
+var searchType = program.type;
+var searchTitle = program.name;
+var seasonId = program.season;
+var episodeId = program.episode;
+var watchStatus = program.status.toLowerCase();
+var watchVisibility = program.private ? 'private' : 'public';
+var watchContent = program.message ? program.message : '';
+var postCategory = program.category;
+var postDate = program.date || moment();
 if (searchType == "movie") {
     MovieDB.searchMovie({ query: searchTitle }, function (err, res) {
         var movieResult = res.results[0];
@@ -29,13 +42,19 @@ if (searchType == "movie") {
                 watchName: movieInfo.title,
                 imdbId: movieInfo.imdb_id,
                 watchUrl: movieInfo.homepage,
-                watchImageUrl: movieInfo.poster_path
+                watchImageUrl: movieInfo.poster_path,
+                watchContent: watchContent,
+                watchVisibility: watchVisibility
             });
         });
     });
 }
 else if (searchType == "tv") {
     MovieDB.searchTv({ query: searchTitle }, function (err, res) {
+        if (err) {
+            console.log("Error");
+            console.log(err);
+        }
         var showResult = res.results[0];
         if (seasonId == undefined) {
             MovieDB.tvInfo({ id: showResult.id }, function (err, show) {
@@ -53,17 +72,21 @@ else if (searchType == "tv") {
         else {
             MovieDB.tvInfo({ id: showResult.id }, function (err, showInfo) {
                 MovieDB.tvExternalIds({ id: showResult.id }, function (err, showIds) {
-                    MovieDB.tvEpisodeInfo({ id: showResult.id, season_number: seasonId, episode_number: episodeId }, function (err, episodeInfo) {
-                        saveTvTemplate({
-                            postCategory: postCategory,
-                            watchStatus: watchStatus,
-                            watchName: showResult.name,
-                            watchSeason: seasonId,
-                            watchEpisode: episodeId,
-                            imdbId: showIds.imdb_id,
-                            watchUrl: showInfo.homepage,
-                            watchImageUrl: showInfo.poster_path,
-                            watchEpisodeImageUrl: episodeInfo.still_path
+                    MovieDB.tvSeasonInfo({ id: showResult.id, season_number: seasonId }, function (err, seasonInfo) {
+                        MovieDB.tvEpisodeInfo({ id: showResult.id, season_number: seasonId, episode_number: episodeId }, function (err, episodeInfo) {
+                            saveTvTemplate({
+                                postCategory: postCategory,
+                                watchStatus: watchStatus,
+                                watchName: showResult.name,
+                                watchSeason: seasonId,
+                                watchEpisode: episodeId,
+                                imdbId: showIds.imdb_id,
+                                watchUrl: showInfo.homepage,
+                                watchImageUrl: (seasonInfo.poster_path ? seasonInfo.poster_path : showInfo.poster_path),
+                                watchEpisodeImageUrl: episodeInfo.still_path,
+                                watchContent: watchContent,
+                                watchVisibility: watchVisibility
+                            });
                         });
                     });
                 });
@@ -72,11 +95,13 @@ else if (searchType == "tv") {
     });
 }
 function saveMovieTemplate(info) {
-    var template = "---\nlayout: post\ntitle:  ''\ndate:   {{date}}\ntags: [{{category}}]\nstatus: {{status}}\nmovie_name: \"{{name}}\"\nimdb_id: {{imdb_id}}\nmovie_url: {{movie_url}}\nmovie_image: {{movie_image_url}}\npermalink: /watch/:year/:month/:title/\n---";
-    var data = { formatted: undefined, filename: "watch/movie/" + (info.watchName.replace(/ /g, "-").replace(/[\'\":]/g, "") + "-" + info.watchStatus + ".md").toLowerCase() };
-    data.formatted = template.replace("{{date}}", moment().format("YYYY-MM-DD HH:mm:ss ZZ"))
-        .replace("{{category}}", info.postCategory)
+    var template = "---\nlayout: post\ntitle:  ''\nvisibility: {{post_visibility}}\ndate:   {{date}}\ntags: [{{category}}]\nstatus: {{status}}\nmovie_name: \"{{name}}\"\nimdb_id: {{imdb_id}}\nmovie_url: {{movie_url}}\nmovie_image: {{movie_image_url}}\npermalink: /watch/:year/:month/:title/\n---\n{{content}}";
+    var data = { formatted: undefined, filename: "watch/movie/" + (info.watchName.replace(/ /g, "-").replace(/[\'\":.]/g, "") + "-" + info.watchStatus + ".md").toLowerCase() };
+    data.formatted = template.replace("{{date}}", postDate.format("YYYY-MM-DD HH:mm:ss ZZ"))
+        .replace("{{category}}", "[" + info.postCategory.join(", ") + "]")
+        .replace("{{content}}", info.watchContent)
         .replace("{{status}}", info.watchStatus)
+        .replace("{{post_visibility}}", info.watchVisibility)
         .replace("{{name}}", info.watchName)
         .replace("{{imdb_id}}", info.imdbId)
         .replace("{{movie_url}}", info.watchUrl)
@@ -84,11 +109,13 @@ function saveMovieTemplate(info) {
     saveFile(data);
 }
 function saveTvTemplate(info) {
-    var template = "---\nlayout: post\ntitle:  ''\ndate:   {{date}}\ntags: [{{category}}]\nstatus: {{status}}\nshow_name: \"{{name}}\"\nshow_season: {{show_season}}\nshow_episode: {{show_episode}}\nimdb_id: {{imdb_id}}\nshow_url: {{show_url}}\nshow_image: {{show_image_url}}\nepisode_image: {{episode_image_url}}\npermalink: /watch/:year/:month/:title/\n---";
-    var data = { formatted: undefined, filename: "watch/tv/" + (info.watchName.replace(/ /g, "-").replace(/[\'\"]/g, "") + "-s" + info.watchSeason + "-e" + info.watchEpisode + "-" + info.watchStatus + ".md").toLowerCase() };
-    data.formatted = template.replace("{{date}}", moment().format("YYYY-MM-DD HH:mm:ss ZZ"))
-        .replace("{{category}}", info.postCategory)
+    var template = "---\nlayout: post\ntitle:  ''\nvisibility: {{post_visibility}}\ndate:   {{date}}\ntags: [{{category}}]\nstatus: {{status}}\nshow_name: \"{{name}}\"\nshow_season: {{show_season}}\nshow_episode: {{show_episode}}\nimdb_id: {{imdb_id}}\nshow_url: {{show_url}}\nshow_image: {{show_image_url}}\nepisode_image: {{episode_image_url}}\npermalink: /watch/:year/:month/:title/\n---\n{{content}}";
+    var data = { formatted: undefined, filename: "watch/tv/" + (info.watchName.replace(/ /g, "-").replace(/[\'\":.]/g, "") + "-s" + info.watchSeason + "-e" + info.watchEpisode + "-" + info.watchStatus + ".md").toLowerCase() };
+    data.formatted = template.replace("{{date}}", postDate.format("YYYY-MM-DD HH:mm:ss ZZ"))
+        .replace("{{category}}", info.postCategory.join(", "))
+        .replace("{{content}}", info.watchContent)
         .replace("{{status}}", info.watchStatus)
+        .replace("{{post_visibility}}", info.watchVisibility)
         .replace("{{name}}", info.watchName)
         .replace("{{show_season}}", info.watchSeason)
         .replace("{{show_episode}}", info.watchEpisode)
@@ -99,7 +126,7 @@ function saveTvTemplate(info) {
     saveFile(data);
 }
 function saveFile(data) {
-    fs.writeFile(__dirname + '/../../eddie-personal/_source/_media/' + data.filename, data.formatted, function (err) {
+    fs.writeFile(__dirname + '/../../abode-eddie/jekyll/_source/_media/' + data.filename, data.formatted, function (err) {
         if (err) {
             return console.log(err);
         }
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,41 @@
+#!/usr/bin/env node
+
 import * as moment from 'moment';
 import * as fs from 'fs';
 import * as _ from 'lodash';
+import * as program from 'commander';
 
 var config = require('../config.json');
 
 const MovieDB = require('moviedb')(config.moviedb_key);
 const defaultWatchStatus = "finished";
 
-var postCategory, watchStatus, watchName, imdbId, watchUrl, watchImageUrl;
+program.option('-t, --type <type>', 'What type of watch post it is')
+  .option('-n, --name <name>', 'The name of what is being watched')
+  .option('-s, --season <season>', 'The season number of a tv show', parseInt)
+  .option('-e, --episode <episode>', 'The episode number of a tv show', parseInt)
+  .option('-S, --status <status>', 'The status of the watch post: interested, inprogress or finished', /^(interested|inprogress|finished)$/i, defaultWatchStatus)
+  .option('--private', 'Mark this post as a private post')
+  .option('-m, --message <message>', 'This is the message you want to put in the content of the post')
+  .option('-c, --category <category>', 'These are the categories you want to attach to the post', function list(val) {
+      return val.toLowerCase().split(',');
+  })
+  .option('-d, --date [date]', 'This is the date of the watch post', function datetime(val){
+    return moment(val, "MMM D, YYYY h:mm A");
+  })
+  .parse(process.argv);
 
-var searchType = process.argv[2];
-var searchTitle = process.argv[3];
+var watchName, imdbId, watchUrl, watchImageUrl;
 
-if (searchType == "movie") {
-    postCategory = process.argv[4] ? process.argv[4].toLowerCase() : undefined;
-    watchStatus = process.argv[5] ? process.argv[5].toLowerCase() : defaultWatchStatus;
-    
-} else if (searchType == "tv") {
-    var seasonId = process.argv[4];
-    var episodeId = process.argv[5];
-    postCategory = process.argv[6] ? process.argv[6].toLowerCase() : undefined;
-    watchStatus = process.argv[7] ? process.argv[7].toLowerCase() : defaultWatchStatus;
-}
+var searchType = program.type;
+var searchTitle = program.name;
+var seasonId = program.season;
+var episodeId = program.episode;
+var watchStatus = program.status.toLowerCase();
+var watchVisibility = program.private ? 'private' : 'public';
+var watchContent = program.message ? program.message : '';
+var postCategory = program.category;
+var postDate = program.date || moment();
 
 if (searchType == "movie") {
     MovieDB.searchMovie({ query: searchTitle }, (err, res) => {
@@ -35,12 +49,18 @@ if (searchType == "movie") {
                 watchName: movieInfo.title,
                 imdbId: movieInfo.imdb_id,
                 watchUrl: movieInfo.homepage,
-                watchImageUrl: movieInfo.poster_path
+                watchImageUrl: movieInfo.poster_path,
+                watchContent: watchContent,
+                watchVisibility: watchVisibility
             })
         });
     });
 } else if (searchType == "tv") {
     MovieDB.searchTv({ query: searchTitle }, (err, res) => {
+        if (err) {
+            console.log("Error");
+            console.log(err);
+        }
         var showResult = res.results[0];
 
         if (seasonId == undefined) {
@@ -59,19 +79,24 @@ if (searchType == "movie") {
 
                 MovieDB.tvExternalIds({ id: showResult.id }, (err, showIds) => {
 
-                    MovieDB.tvEpisodeInfo({ id: showResult.id, season_number: seasonId, episode_number: episodeId }, (err, episodeInfo) => {
+                    MovieDB.tvSeasonInfo({ id: showResult.id, season_number: seasonId }, (err, seasonInfo) => {
 
-                        saveTvTemplate({
-                            postCategory: postCategory,
-                            watchStatus: watchStatus,
-                            watchName: showResult.name,
-                            watchSeason: seasonId,
-                            watchEpisode: episodeId,
-                            imdbId: showIds.imdb_id,
-                            watchUrl: showInfo.homepage,
-                            watchImageUrl: showInfo.poster_path,
-                            watchEpisodeImageUrl: episodeInfo.still_path
-                        })
+                        MovieDB.tvEpisodeInfo({ id: showResult.id, season_number: seasonId, episode_number: episodeId }, (err, episodeInfo) => {
+
+                            saveTvTemplate({
+                                postCategory: postCategory,
+                                watchStatus: watchStatus,
+                                watchName: showResult.name,
+                                watchSeason: seasonId,
+                                watchEpisode: episodeId,
+                                imdbId: showIds.imdb_id,
+                                watchUrl: showInfo.homepage,
+                                watchImageUrl: (seasonInfo.poster_path ? seasonInfo.poster_path : showInfo.poster_path),
+                                watchEpisodeImageUrl: episodeInfo.still_path,
+                                watchContent: watchContent,
+                                watchVisibility: watchVisibility
+                            })
+                        });
                     });
                 });
             });
@@ -81,13 +106,15 @@ if (searchType == "movie") {
 
 
 function saveMovieTemplate(info) {
-    var template = "---\nlayout: post\ntitle:  ''\ndate:   {{date}}\ntags: [{{category}}]\nstatus: {{status}}\nmovie_name: \"{{name}}\"\nimdb_id: {{imdb_id}}\nmovie_url: {{movie_url}}\nmovie_image: {{movie_image_url}}\npermalink: /watch/:year/:month/:title/\n---";
+    var template = "---\nlayout: post\ntitle:  ''\nvisibility: {{post_visibility}}\ndate:   {{date}}\ntags: [{{category}}]\nstatus: {{status}}\nmovie_name: \"{{name}}\"\nimdb_id: {{imdb_id}}\nmovie_url: {{movie_url}}\nmovie_image: {{movie_image_url}}\npermalink: /watch/:year/:month/:title/\n---\n{{content}}";
 
-    var data = { formatted: undefined, filename: "watch/movie/" + (info.watchName.replace(/ /g, "-").replace(/[\'\":]/g, "") + "-" + info.watchStatus + ".md").toLowerCase() };
+    var data = { formatted: undefined, filename: "watch/movie/" + (info.watchName.replace(/ /g, "-").replace(/[\'\":.]/g, "") + "-" + info.watchStatus + ".md").toLowerCase() };
 
-    data.formatted = template.replace("{{date}}", moment().format("YYYY-MM-DD HH:mm:ss ZZ"))
-    .replace("{{category}}", info.postCategory)
+    data.formatted = template.replace("{{date}}", postDate.format("YYYY-MM-DD HH:mm:ss ZZ"))
+    .replace("{{category}}", "[" + info.postCategory.join(", ") + "]")
+    .replace("{{content}}", info.watchContent)
     .replace("{{status}}", info.watchStatus)
+    .replace("{{post_visibility}}", info.watchVisibility)
     .replace("{{name}}", info.watchName)
     .replace("{{imdb_id}}", info.imdbId)
     .replace("{{movie_url}}", info.watchUrl)
@@ -98,13 +125,15 @@ function saveMovieTemplate(info) {
 }
 
 function saveTvTemplate(info) {
-    var template = "---\nlayout: post\ntitle:  ''\ndate:   {{date}}\ntags: [{{category}}]\nstatus: {{status}}\nshow_name: \"{{name}}\"\nshow_season: {{show_season}}\nshow_episode: {{show_episode}}\nimdb_id: {{imdb_id}}\nshow_url: {{show_url}}\nshow_image: {{show_image_url}}\nepisode_image: {{episode_image_url}}\npermalink: /watch/:year/:month/:title/\n---";
+    var template = "---\nlayout: post\ntitle:  ''\nvisibility: {{post_visibility}}\ndate:   {{date}}\ntags: [{{category}}]\nstatus: {{status}}\nshow_name: \"{{name}}\"\nshow_season: {{show_season}}\nshow_episode: {{show_episode}}\nimdb_id: {{imdb_id}}\nshow_url: {{show_url}}\nshow_image: {{show_image_url}}\nepisode_image: {{episode_image_url}}\npermalink: /watch/:year/:month/:title/\n---\n{{content}}";
 
-    var data = { formatted: undefined, filename: "watch/tv/" + (info.watchName.replace(/ /g, "-").replace(/[\'\"]/g, "") + "-s" + info.watchSeason + "-e" + info.watchEpisode + "-" + info.watchStatus + ".md").toLowerCase() };
+    var data = { formatted: undefined, filename: "watch/tv/" + (info.watchName.replace(/ /g, "-").replace(/[\'\":.]/g, "") + "-s" + info.watchSeason + "-e" + info.watchEpisode + "-" + info.watchStatus + ".md").toLowerCase() };
 
-    data.formatted = template.replace("{{date}}", moment().format("YYYY-MM-DD HH:mm:ss ZZ"))
-    .replace("{{category}}", info.postCategory)
+    data.formatted = template.replace("{{date}}", postDate.format("YYYY-MM-DD HH:mm:ss ZZ"))
+    .replace("{{category}}", info.postCategory.join(", "))
+    .replace("{{content}}", info.watchContent)
     .replace("{{status}}", info.watchStatus)
+    .replace("{{post_visibility}}", info.watchVisibility)
     .replace("{{name}}", info.watchName)
     .replace("{{show_season}}", info.watchSeason)
     .replace("{{show_episode}}", info.watchEpisode)
@@ -118,7 +147,7 @@ function saveTvTemplate(info) {
 }
 
 function saveFile(data) {
-    fs.writeFile(__dirname + '/../../eddie-personal/_source/_media/' + data.filename, data.formatted, function(err) {
+    fs.writeFile(__dirname + '/../../abode-eddie/jekyll/_source/_media/' + data.filename, data.formatted, function(err) {
         if(err) {
             return console.log(err);
         }
